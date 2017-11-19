c05e99c5e86eed6051e40121e2b814c3c687476a
Wojciech Francuzik <wolass@gmail.com>
2017-11-19 03:06:58 +0100

    changes to the paper

---
+++ analysis/requirements/ExpRevClinImmun.R
@@ -0,0 +1,54 @@
+# Review: Expert Review
+expert-review-logoWord limit: The word limit for Reviews is 7,000 words (not including figures, tables or references).
+
+Every article must contain:
+
+Title: All article types should have a concise, informative title that contains no brand names.
+
+Authors’ names and affiliation: Including address, academic qualifications and job titles of all authors, as well as telephone number and email address of the author for correspondence on a separate cover sheet as the peer reviewers will be blinded to the authors’ identity. Please note that only the address of the first author of the article will appear on Medline/PubMed, not necessarily the corresponding author.
+
+Structured abstract (maximum 200 words): The aim of the abstract is to draw in the interested reader and provide an accurate reflection of the content of the paper. We therefore request the following structure is followed for full-length review articles:
+
+  Introduction: Authors are required to describe the significance of the topic under discussion.
+Areas covered: Authors are required to describe the research discussed and the literature search undertaken.
+Expert commentary: The author’s expert view on the current status of the field under discussion.
+References must not be included in the abstract.
+
+Keywords: A brief list of keywords, in alphabetical order, is required to assist indexers in cross-referencing. The keywords will encompass the therapeutic area, mechanism(s) of action, key compounds and so on.
+
+Body of the article:
+
+  Introduction: Incorporating basic background information on the area under review.
+Body: Body of the review paper covering the subject under review, using numbered subsections.
+Conclusion: The conclusion for all articles should contain a brief summary of the data presented in the article. Please note that this section is meant to be distinct from, and appear before the ‘Expert opinion’ section.
+Expert Commentary: 500-1000 words (included in overall word count).
+
+To distinguish the articles published in the Expert Review series, authors must provide an additional section entitled ‘Expert Commentary’. This section affords authors the opportunity to provide their interpretation of the data presented in the article and discuss the developments that are likely to be important in the future, and the avenues of research likely to become exciting as further studies yield more detailed results. The intention is to go beyond a conclusion and should not simply summarise the paper. Authors should answer the following:
+
+  What are the key weaknesses in clinical management so far?
+What potential does further research hold? What is the ultimate goal in this field?
+What research or knowledge is needed to achieve this goal and what is the biggest challenge in this goal being achieved?
+Is there any particular area of the research you are finding of interest at present?
+Please note that ‘opinions’ are encouraged in the Expert commentary section, and, as such, referees are asked to keep this in mind when peer reviewing the manuscript.
+
+Five-year view
+
+Authors are challenged to include a speculative viewpoint on how the field will have evolved five years from the point at which the review was written.
+
+Key issues
+
+An executive summary of the authors’ main points (bulleted) is very useful for time-constrained readers requiring a rapidly accessible overview.
+
+Example:
+
+  Key issues
+
+Matrix-Assisted Laser Desorption/Ionization  Mass Spectrometry Imaging (MALDI-MSI) is able to provide a molecular insight of the samples, detecting the presence of a great variety of analytes directly on-tissue and showing their spatial distribution across the tissue section.
+A typical MALDI-MSI dataset is composed of mass spectra corresponding to pixels of the digitalized tissue slice and it is structured as a data cube, in which every mass-to-charge ratio (m/z) value is associated with a molecular image showing the localization of that specific analyte on-tissue. The dimensionality of the data strictly depends on the mass resolution and on the spatial distribution (i.e. number of pixels) of the spectral acquisition.
+The preprocessing phase ensures that all the spectra of the dataset are brought to the same scale, allowing fair comparisons between spectra/pixels within the same tissue section and among different analyses, by discarding all the fluctuations associated with instrument performances and sample heterogeneity.
+Machine learning comprises a series of algorithms aimed at learning features from data and subsequently returning the results by exploiting patterns or regularities within the data. This approach is widely employed in several fields, for clustering (unsupervised) and classification (supervised) purposes. While the former do not require any prior knowledge about the label of the data and return hidden patterns within the data, the latter exploit the known input data in order to make predictions onto new unlabeled data.
+References: A maximum of 100 references is suggested. Ensure that all key work relevant to the topic under discussion is cited in the text and listed in the bibliography. Reference to unpublished data should be kept to a minimum and authors must obtain a signed letter of permission from cited persons to use unpublished results or personal communications in the manuscript.
+
+Annotated bibliography: Important references should be highlighted with a one/two star system and brief annotations should be given (see the journal’s Instructions for Authors page for examples and for a more detailed description of our referencing style).
+
+Figures and Tables: Up to 5 figures and 5 tables are permitted. For further information on tables and figures, please see our formatting guide.
